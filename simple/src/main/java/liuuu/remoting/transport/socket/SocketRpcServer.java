8d62ab27bea3686666f0d3378e2f491ec9e02a25
liuuu <liugongmingg@163.com>
2024-03-22 16:48:16 +0800

    实现服务器端的利用Socket协议传输的Rpc调用方法

--- simple/src/main/java/liuuu/remoting/transport/socket/SocketRpcServer.java
+++ simple/src/main/java/liuuu/remoting/transport/socket/SocketRpcServer.java
@@ -1,19 +1,24 @@
 package liuuu.remoting.transport.socket;
 
 import jdk.internal.module.ServicesCatalog;
+import liuuu.config.CustomShutdownHook;
 import liuuu.config.RpcServiceConfig;
 import liuuu.factory.SingletonFactory;
 import liuuu.provider.ServiceProvider;
 import liuuu.provider.impl.ZkServiceProviderImpl;
 import liuuu.utils.concurrent.threadpool.ThreadPoolFactoryUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.sound.sampled.Port;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 
+
+@Slf4j
 public class SocketRpcServer {
     private final ExecutorService threadPool;
     private final ServiceProvider serviceProvider;
@@ -31,7 +36,13 @@ public class SocketRpcServer {
         try (ServerSocket server = new ServerSocket()) {
             String host = InetAddress.getLocalHost().getHostAddress();
             server.bind(new InetSocketAddress(host, PORT));
-            CustomShutdownHook.get
+            CustomShutdownHook.getCustomShutdownHook().clearAll();
+            Socket socket;
+            while ((socket = server.accept()) != null) {
+                threadPool.execute(new SocketRpcRequestHandlerRunnable(socket));
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
         }
     }
 }
