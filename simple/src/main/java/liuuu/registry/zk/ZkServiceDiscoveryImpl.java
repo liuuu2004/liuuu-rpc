4550731973a0212a62cd826e06a5785310078d7c
liuuu <liugongmingg@163.com>
2024-03-25 11:16:08 +0800

    实现服务查找方法

--- simple/src/main/java/liuuu/registry/zk/ZkServiceDiscoveryImpl.java
+++ simple/src/main/java/liuuu/registry/zk/ZkServiceDiscoveryImpl.java
@@ -1,11 +1,19 @@
 package liuuu.registry.zk;
 
+import liuuu.enums.LoadBalanceEnum;
+import liuuu.enums.RpcErrorMessageEnum;
+import liuuu.exception.RpcException;
+import liuuu.extension.ExtensionLoader;
 import liuuu.loadbalance.LoadBalance;
 import liuuu.registry.ServiceDiscovery;
+import liuuu.registry.zk.util.CuratorUtils;
 import liuuu.remoting.dto.RpcRequest;
+import liuuu.utils.CollectionUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 
 /**
  * 基于Zookeeper实现的注册发现
@@ -16,8 +24,29 @@ public class ZkServiceDiscoveryImpl implements ServiceDiscovery {
 
     private final LoadBalance loadBalance;
 
+    public ZkServiceDiscoveryImpl(LoadBalance loadBalance) {
+        this.loadBalance = ExtensionLoader.getExtensionLoader(LoadBalance.class).getExtension(LoadBalanceEnum.LOAD_BALANCE.getName());
+    }
+
+    /**
+     * 实现服务查找方法
+     * @param rpcRequest  Rpc服务请求
+     * @return
+     */
     @Override
     public InetSocketAddress lookupService(RpcRequest rpcRequest) {
-        return null;
+        String rpcServiceName = rpcRequest.getRpcServiceName();
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        List<String> serviceUrlList = CuratorUtils.getChildrenNodes(zkClient, rpcServiceName);
+        if (CollectionUtil.isEmpty(serviceUrlList)) {
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_CAN_NOT_BE_FOUND);
+        }
+
+        // 负载均衡
+        String targetServiceUrl = loadBalance.selectServiceAddress(serviceUrlList, rpcRequest);
+        String[] socketAddressArray = targetServiceUrl.split(":");
+        String host = socketAddressArray[0];
+        int port = Integer.parseInt(socketAddressArray[1]);
+        return new InetSocketAddress(host, port);
     }
 }
