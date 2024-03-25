0de3af8420389148d597381a773b2d8da9eeed3f
liuuu <liugongmingg@163.com>
2024-03-25 22:22:15 +0800

    利用注解实现注册/消费服务

---
+++ simple/src/main/java/liuuu/spring/SpringBeanPostProcessor.java
@@ -0,0 +1,80 @@
+package liuuu.spring;
+
+
+import liuuu.annotation.RpcReference;
+import liuuu.annotation.RpcService;
+import liuuu.config.RpcServiceConfig;
+import liuuu.enums.RpcRequestTransportEnum;
+import liuuu.extension.ExtensionLoader;
+import liuuu.factory.SingletonFactory;
+import liuuu.provider.ServiceProvider;
+import liuuu.provider.impl.ZkServiceProviderImpl;
+import liuuu.proxy.RpcClientProxy;
+import liuuu.remoting.transport.RpcRequestTransport;
+import lombok.SneakyThrows;
+import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+
+@Slf4j
+@Component
+public class SpringBeanPostProcessor implements BeanPostProcessor {
+
+    private final ServiceProvider serviceProvider;
+
+    private final RpcRequestTransport rpcClient;
+
+    public SpringBeanPostProcessor() {
+        this.serviceProvider = SingletonFactory.getInstance(ZkServiceProviderImpl.class);
+        this.rpcClient = ExtensionLoader.getExtensionLoader(RpcRequestTransport.class).getExtension(RpcRequestTransportEnum.NETTY.name());
+    }
+
+
+    /**
+     * 初始化前发起进程
+     * @param bean the new bean instance
+     * @param beanName the name of the bean
+     * @return
+     */
+    @SneakyThrows
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        if (bean.getClass() .isAnnotationPresent(RpcService.class)) {
+            RpcService rpcService = bean.getClass().getAnnotation(RpcService.class);
+            RpcServiceConfig rpcServiceConfig = RpcServiceConfig.builder()
+                    .group(rpcService.group())
+                    .version(rpcService.version())
+                    .service(bean)
+                    .build();
+            serviceProvider.publishService(rpcServiceConfig);
+        }
+        return bean;
+    }
+
+
+    public Object PostProcessAfterInitialization(Object bean, String beanName) {
+        Class<?> targetClass = bean.getClass();
+        Field[] declaredFields = targetClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            RpcReference rpcReference = declaredField.getAnnotation(RpcReference.class);
+            if (rpcReference != null) {
+                RpcServiceConfig rpcServiceConfig = RpcServiceConfig.builder()
+                        .group(rpcReference.group())
+                        .version(rpcReference.version())
+                        .build();
+                RpcClientProxy rpcClientProxy = new RpcClientProxy(rpcClient, rpcServiceConfig);
+                Object clientProxy = rpcClientProxy.getProxy(declaredField.getType());
+                declaredField.setAccessible(true);
+                try {
+                    declaredField.set(bean, clientProxy);
+                } catch (IllegalAccessException e) {
+                    log.error(e.getMessage());
+                }
+            }
+        }
+        return bean;
+    }
+}
