8337a2121dfa3bb5998acfca037594c806f55221
liuuu <liugongmingg@163.com>
2024-03-25 16:11:11 +0800

    完善动态代理机制框架

--- simple/src/main/java/liuuu/proxy/RpcClientProxy.java
+++ simple/src/main/java/liuuu/proxy/RpcClientProxy.java
@@ -1,11 +1,22 @@
 package liuuu.proxy;
 
+import io.protostuff.Rpc;
 import liuuu.config.RpcServiceConfig;
+import liuuu.enums.RpcErrorMessageEnum;
+import liuuu.enums.RpcResponseCodeEnum;
+import liuuu.exception.RpcException;
+import liuuu.remoting.dto.RpcRequest;
+import liuuu.remoting.dto.RpcResponse;
 import liuuu.remoting.transport.RpcRequestTransport;
+import liuuu.remoting.transport.netty.server.NettyRpcServer;
+import liuuu.remoting.transport.socket.SocketRpcClient;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.UUID;
 
 
 @Slf4j
@@ -27,7 +38,67 @@ public class RpcClientProxy implements InvocationHandler {
      * @return
      * @param <T>
      */
+    @SuppressWarnings("unchecked")
     public <T> T getProxy(Class<T> clazz) {
         return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, this);
+    }
+
+
+    /**
+     * 代理调用方法
+     * @param proxy the proxy instance that the method was invoked on
+     *
+     * @param method the {@code Method} instance corresponding to
+     * the interface method invoked on the proxy instance.  The declaring
+     * class of the {@code Method} object will be the interface that
+     * the method was declared in, which may be a superinterface of the
+     * proxy interface that the proxy class inherits the method through.
+     *
+     * @param args an array of objects containing the values of the
+     * arguments passed in the method invocation on the proxy instance,
+     * or {@code null} if interface method takes no arguments.
+     * Arguments of primitive types are wrapped in instances of the
+     * appropriate primitive wrapper class, such as
+     * {@code java.lang.Integer} or {@code java.lang.Boolean}.
+     *
+     * @return
+     */
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) {
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .methodName(method.getName())
+                .parameters(args)
+                .interfaceName(method.getDeclaringClass().getName())
+                .paramTypes(method.getParameterTypes())
+                .requestId(UUID.randomUUID().toString())
+                .group(rpcServiceConfig.getGroup())
+                .version(rpcServiceConfig.getVersion())
+                .build();
+        RpcResponse<Object> rpcResponse = null;
+        if (rpcRequestTransport instanceof SocketRpcClient) {
+            rpcResponse = (RpcResponse<Object>) rpcRequestTransport.sendRpcRequest(rpcRequest);
+        }
+        this.check(rpcResponse, rpcRequest);
+        return rpcResponse.getData();
+    }
+
+    private void check(RpcResponse rpcResponse, RpcRequest rpcRequest) {
+
+        // 无响应
+        if (rpcResponse == null) {
+            throw new RpcException(RpcErrorMessageEnum.SERVER_INVOCATION_FAILURE, INTERFACE_NAME + ":" +rpcRequest.getInterfaceName());
+        }
+
+        // 收发id不匹配
+        if (!rpcResponse.getRequestId().equals(rpcRequest.getRequestId())) {
+            throw new RpcException(RpcErrorMessageEnum.REQUEST_NOT_MATCH_RESPONSE, INTERFACE_NAME + ":" + rpcRequest.getInterfaceName());
+        }
+
+        // 无响应码或响应码表示不成功
+        if (rpcResponse.getCode() == null || !rpcResponse.getCode().equals(RpcResponseCodeEnum.SUCCESS)) {
+            throw new RpcException(RpcErrorMessageEnum.SERVER_INVOCATION_FAILURE, INTERFACE_NAME + ":" + rpcRequest.getInterfaceName());
+        }
     }
 }
